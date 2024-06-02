460dc48ba5d673ed7a7494c722a95b0c57623512
maybemay45 <163439175+maybemay45@users.noreply.github.com>
2024-06-02 20:54:54 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
+#include <QtWidgets/qapplication.h>
 #include "mainwindow.h"
-#include <QtWidgets/QApplication>
 
-#include <QFile>
-#include <QString>
-#include <QDebug>
-#include <QTextStream>
-
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
     return a.exec();
 }
